20e9ec9dc6d2d2fbbf1cb398aa12edfd5046fee5
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2020-01-18 01:42:14 +0300

    Add files via upload

---
+++ WinFormTez/WinFormTez/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WinFormTez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBoxVeri = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxOran = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAzalan = new System.Windows.Forms.TextBox();
+            this.textBoxArtan = new System.Windows.Forms.TextBox();
+            this.labelAzalan = new System.Windows.Forms.Label();
+            this.labelArtan = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(54, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(767, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KELİMELERİN YILLARA GÖRE DAĞILIMLARI";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(34, 133);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Yillar";
+            series2.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(599, 339);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBoxVeri
+            // 
+            this.comboBoxVeri.FormattingEnabled = true;
+            this.comboBoxVeri.Location = new System.Drawing.Point(34, 95);
+            this.comboBoxVeri.MaxDropDownItems = 20;
+            this.comboBoxVeri.Name = "comboBoxVeri";
+            this.comboBoxVeri.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxVeri.TabIndex = 2;
+            this.comboBoxVeri.SelectedIndexChanged += new System.EventHandler(this.comboBoxVeri_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(678, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kelimelerin Yıllara Göre Kullanım Oranı";
+            // 
+            // comboBoxOran
+            // 
+            this.comboBoxOran.FormattingEnabled = true;
+            this.comboBoxOran.Location = new System.Drawing.Point(681, 162);
+            this.comboBoxOran.Name = "comboBoxOran";
+            this.comboBoxOran.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxOran.TabIndex = 4;
+            this.comboBoxOran.SelectedIndexChanged += new System.EventHandler(this.comboBoxOran_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(678, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // textBoxAzalan
+            // 
+            this.textBoxAzalan.Location = new System.Drawing.Point(681, 234);
+            this.textBoxAzalan.Multiline = true;
+            this.textBoxAzalan.Name = "textBoxAzalan";
+            this.textBoxAzalan.Size = new System.Drawing.Size(121, 238);
+            this.textBoxAzalan.TabIndex = 6;
+            // 
+            // textBoxArtan
+            // 
+            this.textBoxArtan.Location = new System.Drawing.Point(821, 234);
+            this.textBoxArtan.Multiline = true;
+            this.textBoxArtan.Name = "textBoxArtan";
+            this.textBoxArtan.Size = new System.Drawing.Size(121, 238);
+            this.textBoxArtan.TabIndex = 7;
+            // 
+            // labelAzalan
+            // 
+            this.labelAzalan.AutoSize = true;
+            this.labelAzalan.Location = new System.Drawing.Point(684, 209);
+            this.labelAzalan.Name = "labelAzalan";
+            this.labelAzalan.Size = new System.Drawing.Size(35, 13);
+            this.labelAzalan.TabIndex = 8;
+            this.labelAzalan.Text = "label4";
+            // 
+            // labelArtan
+            // 
+            this.labelArtan.AutoSize = true;
+            this.labelArtan.Location = new System.Drawing.Point(833, 209);
+            this.labelArtan.Name = "labelArtan";
+            this.labelArtan.Size = new System.Drawing.Size(35, 13);
+            this.labelArtan.TabIndex = 9;
+            this.labelArtan.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 500);
+            this.Controls.Add(this.labelArtan);
+            this.Controls.Add(this.labelAzalan);
+            this.Controls.Add(this.textBoxArtan);
+            this.Controls.Add(this.textBoxAzalan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxOran);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxVeri);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBoxVeri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxOran;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAzalan;
+        private System.Windows.Forms.TextBox textBoxArtan;
+        private System.Windows.Forms.Label labelAzalan;
+        private System.Windows.Forms.Label labelArtan;
+    }
+}
+
---
+++ WinFormTez/WinFormTez/Form1.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormTez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.comboBoxVeri.Items.Clear();
+            this.comboBoxOran.Items.Clear();
+            labelAzalan.Text = "Azalan";
+            labelArtan.Text = "Artan";
+
+            StreamReader sr = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/TumKelimeKokleri.txt");
+            string text = sr.ReadLine();
+            sr.Close();
+            string[] kelimeler = text.Split(' ');
+            comboBoxOran.SelectedText = "--Oran Seçiniz--";
+            for (int i = 0; i < 100; i+=10)
+            {
+                this.comboBoxOran.Items.Add("%"+i+"-"+"%"+(i+10));
+            }
+            comboBoxVeri.SelectedText = "--Kelime Seçiniz--";
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                this.comboBoxVeri.Items.Add(kelimeler[i]);
+            }
+           
+            //this.chart1.Series["Series1"].Points.AddY(10);
+        }
+
+        private void comboBoxVeri_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisY.IsMarginVisible = false;
+            //this.chart1.ChartAreas[0].AxisY.Minimum = 0;
+            this.chart1.Series["Yillar"].Points.AddY(0);
+          
+            int sayac=0,sayac2 = 0,sayac3=0,sayac4=0;
+            string secilenDeger = this.comboBoxVeri.Text;
+            StreamReader sr2 = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/1945-1962-KelimeKok.txt");
+            string text2 = sr2.ReadLine();
+            StreamReader sr3 = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/1963-1980-KelimeKok.txt");
+            string text3 = sr3.ReadLine();
+            StreamReader sr4 = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/1981-1999-KelimeKok.txt");
+            string text4 = sr4.ReadLine();
+            StreamReader sr5 = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/2000-2019-KelimeKok.txt");
+            string text5 = sr5.ReadLine();
+          
+            sr2.Close();
+            sr3.Close();
+            sr4.Close();
+            sr5.Close();
+            string[] kelimeler2 = text2.Split(' ');
+            string[] kelimeler3 = text3.Split(' ');
+            string[] kelimeler4 = text4.Split(' ');
+            string[] kelimeler5 = text5.Split(' ');
+       
+          
+
+            for (int i = 0; i < kelimeler2.Length; i++)
+            {
+                if ( secilenDeger== kelimeler2[i])
+                {
+                    sayac++;
+                }
+            }
+            for (int i = 0; i < kelimeler3.Length; i++)
+            {
+                if (secilenDeger == kelimeler3[i])
+                {
+                    sayac2++;
+                }
+            }
+            for (int i = 0; i < kelimeler4.Length; i++)
+            {
+                if (secilenDeger == kelimeler4[i])
+                {
+                    sayac3++;
+                }
+            }
+            for (int i = 0; i < kelimeler5.Length; i++)
+            {
+                if (secilenDeger == kelimeler5[i])
+                {
+                    sayac4++;
+                }
+            }
+        
+            
+
+            if (sayac>sayac2&& sayac > sayac3 && sayac > sayac4 )
+            {
+                this.chart1.ChartAreas[0].AxisY.Maximum = sayac;
+            }
+            if (sayac2> sayac&& sayac2 > sayac3 && sayac2 > sayac4)
+            {
+                this.chart1.ChartAreas[0].AxisY.Maximum = sayac2;
+            }
+            if (sayac3 > sayac2 && sayac3 > sayac && sayac3> sayac4)
+            {
+                this.chart1.ChartAreas[0].AxisY.Maximum = sayac3;
+            }
+            if (sayac4> sayac2 && sayac4> sayac3 && sayac4 > sayac)
+            {
+                this.chart1.ChartAreas[0].AxisY.Maximum = sayac4;
+            }
+
+
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+           
+
+            this.chart1.Series["Yillar"].Points.Add(sayac);
+            this.chart1.Series["Yillar"].Points.Add(sayac2);
+            this.chart1.Series["Yillar"].Points.Add(sayac3);
+            this.chart1.Series["Yillar"].Points.Add(sayac4);
+            chart1.Series["Yillar"].Points[0].AxisLabel = "1945-1962";
+            chart1.Series["Yillar"].Points[1].AxisLabel = "1963-1980";
+            chart1.Series["Yillar"].Points[2].AxisLabel = "1981-1999";
+            chart1.Series["Yillar"].Points[3].AxisLabel = "2000-2019";
+            chart1.Series["Yillar"].Points[0].Color = Color.Blue;
+            chart1.Series["Yillar"].Points[1].Color = Color.Green;
+            chart1.Series["Yillar"].Points[2].Color = Color.Red;
+            chart1.Series["Yillar"].Points[3].Color = Color.Orange;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Angle = -70;
+
+        }
+
+        private void comboBoxOran_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxArtan.ScrollBars = ScrollBars.Vertical;
+            textBoxAzalan.ScrollBars = ScrollBars.Vertical;
+            int sayac=0;
+            int secilenId = this.comboBoxOran.SelectedIndex;
+            StreamReader sr = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/ArtanOranlar.txt");
+            string text = sr.ReadLine();
+            sr.Close();
+            string[] artankelimeOranlari = text.Split(' ');
+
+            StreamReader sr2 = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/AzalanOranlar.txt");
+            string text2 = sr2.ReadLine();
+            sr.Close();
+            string[] azalankelimeOranlari = text2.Split(' ');
+            textBoxAzalan.Clear();
+            textBoxArtan.Clear();
+            //for (int i = 0; i < artankelimeOranlari.Length - 1; i += 2)
+            //{
+            //    if (secilenId == 0)
+            //    {
+            //        labelAzalan.Text = "%0-%10 Artma";
+            //        if (Convert.ToDecimal(artankelimeOranlari[i]) > 0 && Convert.ToDecimal(artankelimeOranlari[i]) <= 10)
+            //        {
+            //            textBoxArtan.Text += artankelimeOranlari[i];
+            //        }
+
+
+
+            //    }
+            //}
+
+            //Azalan Oranların Getirilmesi
+            for (int i = 0; i < azalankelimeOranlari.Length - 1; i += 2)
+            {
+
+                if (secilenId == 0)
+                {
+
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 0 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 10)
+                    {
+
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%0-%10  azalan";
+
+                }
+
+                else if (secilenId == 1)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 10 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 20)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%10-%20  azalan";
+                }
+                else if (secilenId == 2)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 20 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 30)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%20-%30  azalan";
+                }
+                else if (secilenId == 3)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 30 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 40)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%30-%40  azalan";
+                }
+                else if (secilenId == 4)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 40 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 50)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%40-%50 azalan";
+                }
+                else if (secilenId == 5)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 50 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 60)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%50-%60  azalan";
+                }
+                else if (secilenId == 6)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 60 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 70)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%60-%70  azalan";
+                }
+                else if (secilenId == 7)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 70 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 80)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%70-%80  azalan";
+                }
+                else if (secilenId == 8)
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 80 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 90)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%80-%90  azalan";
+                }
+
+                else
+                {
+
+                    if (Convert.ToDecimal(azalankelimeOranlari[i]) > 90 && Convert.ToDecimal(azalankelimeOranlari[i]) <= 100)
+                    {
+                        textBoxAzalan.Text += azalankelimeOranlari[i + 1] + ": " + " %" + azalankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelAzalan.Text = "%90-%100  azalan";
+                }
+
+            }
+
+            for (int i = 0; i < artankelimeOranlari.Length-1; i+=2)
+            {
+
+                if (secilenId == 0)
+                {
+
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 0 && Convert.ToDecimal(artankelimeOranlari[i]) <= 10)
+                    {
+
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%0-%10  artan";
+
+                }
+
+                else if (secilenId == 1)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 10 && Convert.ToDecimal(artankelimeOranlari[i]) <= 20)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%10-%20  artan";
+                }
+                else if (secilenId == 2)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 20 && Convert.ToDecimal(artankelimeOranlari[i]) <= 30)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%20-%30  artan";
+                }
+                else if (secilenId == 3)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 30 && Convert.ToDecimal(artankelimeOranlari[i]) <= 40)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%30-%40  artan";
+                }
+                else if (secilenId == 4)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 40 && Convert.ToDecimal(artankelimeOranlari[i]) <= 50)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%40-%50 artan";
+                }
+                else if (secilenId == 5)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 50 && Convert.ToDecimal(artankelimeOranlari[i]) <= 60)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%50-%60  artan";
+                }
+                else if (secilenId == 6)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 60 && Convert.ToDecimal(artankelimeOranlari[i]) <= 70)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%60-%70  artan";
+                }
+                else if (secilenId == 7)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 70 && Convert.ToDecimal(artankelimeOranlari[i]) <= 80)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%70-%80  artan";
+                }
+                else if (secilenId == 8)
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 80 && Convert.ToDecimal(artankelimeOranlari[i]) <= 90)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%80-%90  artan";
+                }
+
+                else
+                {
+
+                    if (Convert.ToDecimal(artankelimeOranlari[i]) > 90 && Convert.ToDecimal(artankelimeOranlari[i]) <= 100)
+                    {
+                        textBoxArtan.Text += artankelimeOranlari[i + 1] + ": " + " %" + artankelimeOranlari[i] + Environment.NewLine;
+                    }
+                    labelArtan.Text = "%90-%100  artan";
+                }
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+        //private void btnHesapla_Click(object sender, EventArgs e)
+        //{
+        //    StreamReader sr = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/TumKelimeKokleri.txt");
+        //    string text = sr.ReadLine();
+        //    StreamReader sr2 = new StreamReader("C:/Users/kulas/Desktop/WinFormTez/Kelimeler/1945-1962-KelimeKok.txt");
+        //    string text2 = sr2.ReadLine();
+        //    sr.Close();
+        //    string[] kelimeler2 = text2.Split(' ');
+        //    string[] kelimeler = text.Split(' ');
+        //    int[] say;
+        //    say = new int[kelimeler2.Length];
+
+        //    for (int i = 0; i < kelimeler.Length; i++)
+        //    {
+        //        for (int j = 0; j < kelimeler2.Length; j++)
+        //        {
+        //            if (kelimeler[i] == kelimeler2[j])
+        //                say[i] += 1;
+        //        }
+
+        //    }
+        //    for (int i = 0; i < kelimeler2.Length; i++)
+        //    {
+
+        //        textBox1.Text += say[i].ToString();
+
+
+        //    }
+        //}
+    }
+
