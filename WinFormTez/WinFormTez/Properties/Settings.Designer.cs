4d09ad1f351082b089d5619d4199ce8f5cdde56d
sevvalbrt <37674733+sevvalbrt@users.noreply.github.com>
2020-01-18 12:56:09 +0300

    Update Settings.Designer.cs

--- WinFormTez/WinFormTez/Properties/Settings.Designer.cs
+++ WinFormTez/WinFormTez/Properties/Settings.Designer.cs
@@ -4,6 +4,7 @@
 //     Runtime Version:4.0.30319.42000
 //
 //     Changes to this file may cause incorrect behavior and will be lost if
+// 
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
