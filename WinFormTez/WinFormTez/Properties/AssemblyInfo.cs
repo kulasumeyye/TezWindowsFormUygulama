c1fa34d46944438517297d1cd8a4e346cb36a9d1
MeralTd <meraltasdemir77@gmail.com>
2020-01-18 03:44:30 +0300

    Update AssemblyInfo.cs

--- WinFormTez/WinFormTez/Properties/AssemblyInfo.cs
+++ WinFormTez/WinFormTez/Properties/AssemblyInfo.cs
@@ -29,6 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+//
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
